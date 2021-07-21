0613842d15515b733e9ccf3e2cc41e24650c1d3a
baljeet-uimatic <baljeet@uimatic.com>
2021-07-21 14:30:19 +0530

    login functionality added

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_projects/Screens/MainScreen.dart';
 import 'package:flutter_projects/Screens/RegistrationScreen.dart';
+import 'package:flutter_projects/main.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key? key}) : super(key: key);
-
   static const String idScreen = "login";
+
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -15,78 +21,97 @@ class LoginScreen extends StatelessWidget {
           padding: EdgeInsets.all(8.0),
           child: Column(
             children: [
-              SizedBox(height: 35.0,),
-              Image(image:AssetImage("images/logo.png"),
-              width: 390, height: 250, alignment: Alignment.center,
+              SizedBox(
+                height: 35.0,
               ),
-              SizedBox(height: 1,),
-              Text("Login as Rider",
+              Image(
+                image: AssetImage("images/logo.png"),
+                width: 390,
+                height: 250,
+                alignment: Alignment.center,
+              ),
+              SizedBox(
+                height: 1,
+              ),
+              Text(
+                "Login as Rider",
                 style: TextStyle(fontSize: 24, fontFamily: "Brand Bold"),
-              textAlign: TextAlign.center,
+                textAlign: TextAlign.center,
               ),
-              Padding(padding: EdgeInsets.all(20),
-              child: Column(
-                children: [
-                  SizedBox(height: 1,),
-                  TextField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                        labelText: "Email",
-                        labelStyle: TextStyle(
-                            fontSize: 14
-                        ),
-                        hintStyle: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 10
-                        )
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 1,
                     ),
-                    style: TextStyle(
-                        fontSize: 14
+                    TextField(
+                      controller: emailTextEditingController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          labelText: "Email",
+                          labelStyle: TextStyle(fontSize: 14),
+                          hintStyle:
+                              TextStyle(color: Colors.grey, fontSize: 10)),
+                      style: TextStyle(fontSize: 14),
                     ),
-                  ),
-                  SizedBox(height: 1,),
-                  TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        labelText: "Password",
-                        labelStyle: TextStyle(
-                            fontSize: 14
-                        ),
-                        hintStyle: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 10
-                        )
+                    SizedBox(
+                      height: 1,
                     ),
-                    style: TextStyle(
-                        fontSize: 14
+                    TextField(
+                      controller: passwordTextEditingController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          labelText: "Password",
+                          labelStyle: TextStyle(fontSize: 14),
+                          hintStyle:
+                              TextStyle(color: Colors.grey, fontSize: 10)),
+                      style: TextStyle(fontSize: 14),
                     ),
-                  ),
-                  SizedBox(height: 1,),
-                  ElevatedButton(child: const Text('Login'),
-                    onPressed: (){
-                    print("login button clicked");
-                  },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(Colors.yellow),
-                      foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
+                    SizedBox(
+                      height: 1,
+                    ),
+                    ElevatedButton(
+                      child: const Text('Login'),
+                      onPressed: () {
+                        if (!RegExp(
+                                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                            .hasMatch(emailTextEditingController.text)) {
+                          displayMessage(
+                              "Please enter a valid email address", context);
+                        } else if (passwordTextEditingController.text.length <
+                            6) {
+                          displayMessage(
+                              "Password must be at least 6 character long",
+                              context);
+                        } else {
+                          loginAuthenticateUser(context);
+                        }
+                      },
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.yellow),
+                        foregroundColor:
+                            MaterialStateProperty.all<Color>(Colors.black),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(24),
-                        )
+                        )),
                       ),
-                    ),
-                  )
-
-                ],
+                    )
+                  ],
+                ),
               ),
-              ),
-              TextButton(onPressed: (){
-                Navigator.pushNamedAndRemoveUntil(context, RegistrationScreen.idScreen, (route) => false);
-              }, child: Text(
-                "Don't have an Account? Register here."
-              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pushNamedAndRemoveUntil(
+                      context, RegistrationScreen.idScreen, (route) => false);
+                },
+                child: Text("Don't have an Account? Register here."),
                 style: ButtonStyle(
-                  foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
                 ),
               ),
             ],
@@ -94,5 +119,39 @@ class LoginScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  void loginAuthenticateUser(BuildContext context) async {
+    final firebaseUser = (await _firebaseAuth
+            .signInWithEmailAndPassword(
+                email: emailTextEditingController.text,
+                password: passwordTextEditingController.text)
+            .catchError((err) {
+      displayMessage("Error: " + err.toString(), context);
+    }))
+        .user;
+
+    if (firebaseUser != null) {
+      userRef.child(firebaseUser.uid).once().then((DataSnapshot snap) {
+        if (snap.value != null) {
+          Navigator.pushNamedAndRemoveUntil(
+              context, MainScreen.idScreen, (route) => false);
+          displayMessage('You are logged in now.', context);
+        } else {
+          _firebaseAuth.signOut();
+          displayMessage(
+              'No record exists for this user. Please create new account.',
+              context);
+        }
+      });
+    } else {
+      displayMessage("New user account has not been created", context);
+    }
+  }
+
+  displayMessage(String msg, BuildContext context) {
+    Fluttertoast.showToast(msg: msg);
   }
 }
